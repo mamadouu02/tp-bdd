4790a6dad79ea225f058d7e1aaca8cf48c77d4b2
Mamadou <mamadou.thiongane@grenoble-inp.org>
2023-10-19 15:05:49 +0200

    Conquetes

--- conquetes.sql
+++ conquetes.sql
@@ -1,26 +1,17 @@
 CREATE SEQUENCE codeNavireSeq;
-CREATE SEQUENCE codeMission;
-CREATE SEQUENCE codePilote;
-CREATE SEQUENCE codeGalaxie;
+CREATE SEQUENCE codePiloteSeq;
+CREATE SEQUENCE codeGalaxieSeq;
+CREATE SEQUENCE codeMissionSeq;
 
 CREATE TABLE Navire (
-    codeNavire INTEGER DEFAULT codeNavireSeq.nextval PRIMARY KEY,
+    codeNavire INTEGER DEFAULT codeNavireSeq.NEXTVAL PRIMARY KEY,
     rayonAction NUMBER NOT NULL CHECK (rayonAction > 0),
     nbPilotes INTEGER NOT NULL CHECK (nbPilotes > 0),
     vitMax NUMBER NOT NULL CHECK (vitMax > 0)
 );
 
-CREATE TABLE Mission (
-    codeMission INTEGER DEFAULT codeMission.nextval PRIMARY KEY,
-    typeMission VARCHAR(30) REFERENCES TypeMission,
-    dateMission DATE,
-    nbVaisseaux INTEGER,
-    vitMin NUMBER,
-    rayonEng NUMBER
-);
-
 CREATE TABLE Pilote (
-    codePilote INTEGER DEFAULT codePilote.nextval PRIMARY KEY,
+    codePilote INTEGER DEFAULT codePiloteSeq.NEXTVAL PRIMARY KEY,
     nomPilote VARCHAR(30),
     prenom VARCHAR(30),
     age INTEGER,
@@ -28,7 +19,26 @@ CREATE TABLE Pilote (
 );
 
 CREATE TABLE Galaxie (
-    codeGalaxie INTEGER DEFAULT codeGalaxie.nextval PRIMARY KEY,
+    codeGalaxie INTEGER DEFAULT codeGalaxieSeq.NEXTVAL PRIMARY KEY,
     nomGalaxie VARCHAR(30),
     dist NUMBER
 );
+
+CREATE TABLE Mission (
+    codeMission INTEGER DEFAULT codeMissionSeq.NEXTVAL PRIMARY KEY,
+    typeMission VARCHAR(30) REFERENCES TypeMission,
+    dateMission DATE,
+    nbVaisseaux INTEGER,
+    vitMin NUMBER,
+    rayonEng NUMBER
+);
+
+CREATE TABLE Equipage ();
+
+CREATE TABLE NavireTransport ();
+
+CREATE TABLE NavireCombat ();
+
+CREATE TABLE Planete ();
+
+CREATE TABLE TypeMission ();
